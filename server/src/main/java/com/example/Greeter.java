533cf5b08fa44a8e9528369161544cb41b15f8fe
Paul A Rivenburg <parcloud@me.com>
2017-09-11 07:05:03 -0400

    test commit

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -17,7 +17,7 @@ public class Greeter {
    * @return greeting string
    * @author pauliecloud
    */
-  public final String g reet(final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
